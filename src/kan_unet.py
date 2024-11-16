089cf8e6b9e4862b030bd0eec9f939d317fb0687
Yaash1804 <156991731+Yaash1804@users.noreply.github.com>
2024-11-16 21:00:54 +0530

    Update kan_unet.py

--- src/kan_unet.py
+++ src/kan_unet.py
@@ -1,9 +1,3 @@
-"""
-Pytorch implementation of U-Net based on Kolmogorov-Arnold Network, based on the U-Net implementation in https://github.com/milesial/Pytorch-UNet.
-The U-Net model is modified to use the FastKANConvLayer instead of the Conv2d layer in the original implementation.
-The Convolution operation is implemented in https://github.com/XiangboGaoBarry/KA-Conv
-"""
-
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -11,8 +5,6 @@ import torch.nn.functional as F
 from src.fastkanconv import FastKANConvLayer
 
 class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
-
     def __init__(self, in_channels, out_channels, device):
         super().__init__()
         self.in_channels = in_channels
@@ -30,11 +22,9 @@ class DoubleConv(nn.Module):
 
     def forward(self, x):
         return self.double_conv(x)
-    
-class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
 
-    def __init__(self, in_channels, out_channels, device='mps'):
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels, device='cuda'):
         super().__init__()
         self.device = device
         self.maxpool_conv = nn.Sequential(
@@ -44,34 +34,29 @@ class Down(nn.Module):
 
     def forward(self, x):
         return self.maxpool_conv(x)
-    
-class Up(nn.Module):
-    """Upscaling then double conv"""
 
-    def __init__(self, in_channels, out_channels, bilinear=True, device='mps'):
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True, device='cuda'):
         super().__init__()
 
-        # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
             self.conv = DoubleConv(in_channels, out_channels, device=device)
         else:
             self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
-        
+            self.conv = DoubleConv(in_channels, out_channels, device=device)
+
     def forward(self, x1, x2):
         x1 = self.up(x1)
-        # input is CHW
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
-
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
                         diffY // 2, diffY - diffY // 2])
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
-    
+
 class OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, device='cuda'):
         super(OutConv, self).__init__()
         self.conv = FastKANConvLayer(in_channels, out_channels, kernel_size=1)
 
@@ -79,37 +64,35 @@ class OutConv(nn.Module):
         return self.conv(x)
 
 class KANU_Net(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=True, device='mps'):
+    def __init__(self, n_channels, n_classes, bilinear=True, device='cuda'):
         super(KANU_Net, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.bilinear = bilinear
         self.device = device
 
         self.channels = [64, 128, 256, 512, 1024]
 
-        self.inc = (DoubleConv(n_channels, 64, device=self.device))
-        
-        self.down1 = (Down(self.channels[0], self.channels[1], self.device))
-        self.down2 = (Down(self.channels[1], self.channels[2], self.device))
-        self.down3 = (Down(self.channels[2], self.channels[3], self.device))
+        self.inc = DoubleConv(n_channels, 64, device=self.device)
+        self.down1 = Down(self.channels[0], self.channels[1], self.device)
+        self.down2 = Down(self.channels[1], self.channels[2], self.device)
+        self.down3 = Down(self.channels[2], self.channels[3], self.device)
         factor = 2 if bilinear else 1
-        self.down4 = (Down(self.channels[3], self.channels[4] // factor, self.device))
-        self.up1 = (Up(self.channels[4], self.channels[3] // factor, bilinear, self.device))
-        self.up2 = (Up(self.channels[3], self.channels[2] // factor, bilinear, self.device))
-        self.up3 = (Up(self.channels[2], self.channels[1] // factor, bilinear, self.device))
-        self.up4 = (Up(self.channels[1], self.channels[0], bilinear, self.device))
-        self.outc = (OutConv(self.channels[0], n_classes))
+        self.down4 = Down(self.channels[3], self.channels[4] // factor, self.device)
+        self.up1 = Up(self.channels[4], self.channels[3] // factor, bilinear, self.device)
+        self.up2 = Up(self.channels[3], self.channels[2] // factor, bilinear, self.device)
+        self.up3 = Up(self.channels[2], self.channels[1] // factor, bilinear, self.device)
+        self.up4 = Up(self.channels[1], self.channels[0], bilinear, self.device)
+        self.outc = OutConv(self.channels[0], n_classes, self.device)
 
     def forward(self, x):
+        x = x.to(self.device)  # Ensure input is on the correct device
+
         # Encoder
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
-        
-        #Decoder
+
+        # Decoder
         x = self.up1(x5, x4)
         x = self.up2(x, x3)
         x = self.up3(x, x2)
@@ -117,11 +100,9 @@ class KANU_Net(nn.Module):
         logits = self.outc(x)
         return logits
 
-# if __name__ == "__main__":
-#     device = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
-#     # print(device)
-#     model = KANU_Net(3, 1, 'mps').to(device)
-#     # print(model)
-#     x = torch.randn((1, 3, 224, 224)).to(device)
-#     print(model(x).shape)
-
+# Main check
+if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu')
+    model = KANU_Net(3, 1, 'mps').to(device)
+    x = torch.randn((1, 3, 224, 224)).to(device)
+    print(model(x).shape)
